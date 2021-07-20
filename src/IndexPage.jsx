08d8232f19af0a62a3935ef3b45e007b33c68e3a
kokkiemouse <kokkiemouse@fascode.net>
2021-07-20 22:34:36 +0900

    added page

--- src/IndexPage.jsx
+++ src/IndexPage.jsx
@@ -1,10 +1,78 @@
 import React from 'react';
+import Header from './components/Header';
+import {Container,Row,Col,footer} from 'react-bootstrap';
 class IndexPage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Hello, world!</h1>
-            </div>
+            <>
+            <style type="text/css">
+                {
+                    `
+                    .headercont{
+                        height:13vw;
+                        width:100%;
+                        overflow:hidden;
+                        background-color:#434343;
+                    }
+                    .header_back{
+                        background-repeat: repeat;
+                    }
+                    #header_center{
+                        color:white;
+                        text-align:center;
+                        line-height:13vw;
+                        font-size:4.1vw;
+                    }
+                    .midasikun{
+                        font-size:4.9vw;
+                    }
+                    body{
+                        background-color:#f0f2f5;                   
+                    }
+                    .infokun{
+                        margin-top:4vw;
+                        margin-left:2vw;
+                        margin-bottom:3vw;
+                    }
+                    .footerkun{
+                        text-align:center;
+                        background-color:#343434;
+                        color:#fff;
+                    }
+                    `
+                }
+            </style>
+                <Header />
+                <Container fluid className="headercont">
+                    <h1 id="header_center">福島高校SS部情報班</h1>
+                </Container>
+                <Container>
+                    <Row>
+                        
+                        <Col>
+                        <div className="midasikun">Introduce</div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="infokun">
+                         <h2>福島高校SS部情報班は、プログラミングを主体とするクリエイティブな班です。</h2>
+                         <h2>初心者、上級者も大歓迎!!</h2>
+                        </Col>
+                    </Row>
+                    <Row>
+                        
+                        <Col>
+                        <div className="midasikun">How to meet us?</div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="infokun">
+                         <h2>基本的にタブレットPC室で活動しています。班員に積極的に話しかけましょう!</h2>
+                        </Col>
+                    </Row>
+                </Container>
+                <footer className="footerkun">©2021 Fukushima High School SS Club Jouhouhan</footer>
+            </>
         );
     }
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container,Navbar,Nav,Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class Header extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <>
+            <style type="text/css">
+                {`
+                    .header{
+                        background-color: #363636;
+                    }
+                    #firstlinkbt{
+                        margin-left: 8vw;
+                    }
+                    .normallinkbt{
+                        margin-left: 2vw;
+                    }
+                `}
+            </style>
+            <header className="header">
+                <Container>
+                    <Navbar expand="md" variant="dark">
+                        <Navbar.Brand href="/">福島高校SS部情報班</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarResponsive" />
+                        <Navbar.Collapse id="navbarResponsive">
+                            <Nav as="ul" className="al_auto">
+                                <Nav.Item as="li">
+                                    <Button id="firstlinkbt" href="/Info" className="nav_link" activeClassName="active">Info</Button>
+                                    <Button href="/Apps" className="nav_link normallinkbt"  activeClassName="active">Applications</Button>
+                                </Nav.Item>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </Container>
+            </header>
+            </>
+        );
+    }
+}
+export default Header;
