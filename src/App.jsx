f2d96392b47eed18d2308bb543ed5a8fdd1cfb06
kokkiemouse <kokkiemouse@fascode.net>
2021-07-24 10:01:05 +0900

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router,Route } from "react-router-dom";
 import React from 'react';
 import IndexPage from './IndexPage';
 import AppPage from "./pages/AppPage";
+const homeUrl = process.env.PUBLIC_URL;
 class App extends React.Component {
   render() {
     return (
       <Router>
-          <Route exact path="/" component={IndexPage} />
-          <Route path="/Apps" component={AppPage} />
+          <Route exact path={ homeUrl + "/"} component={IndexPage} />
+          <Route path={homeUrl + "/Apps"} component={AppPage} />
       </Router>
     );
   }
