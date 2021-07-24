d4949a18d66a179ac6daa6eac487cfdb0ff5594f
kokkiemouse <kokkiemouse@fascode.net>
2021-07-24 10:15:38 +0900

    fixed
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter as Router,Route } from "react-router-dom";
 import React from 'react';
 import IndexPage from './IndexPage';
 import AppPage from "./pages/AppPage";
+import Routerkun from "./Routerkun"
 const homeUrl = process.env.PUBLIC_URL;
 class App extends React.Component {
   render() {
     return (
-      <Router>
-          <Route exact path={ homeUrl + "/"} component={IndexPage} />
-          <Route path={homeUrl + "/Apps"} component={AppPage} />
-      </Router>
+      <Routerkun />
     );
   }
 }
---
+++ src/Routerkun.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import IndexPage from './IndexPage';
+import AppPage from './pages/AppPage';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+class Routerkun extends React.Component{
+    render(){
+        return(
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={"/"} component={IndexPage} />
+                    <Route path={"/Apps"} component={AppPage} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+export default Routerkun;
