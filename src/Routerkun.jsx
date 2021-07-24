2a3f07dd2563f36e8475c8a4aa6b2502e1d58725
kokkiemouse <kokkiemouse@fascode.net>
2021-07-24 10:43:11 +0900

    fixed

--- src/Routerkun.jsx
+++ src/Routerkun.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import IndexPage from './IndexPage';
 import AppPage from './pages/AppPage';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter , Route, Switch, Redirect } from 'react-router-dom';
 class Routerkun extends React.Component{
     render(){
         return(
-            <Router>
+            <BrowserRouter >
                 <Route exact path={"/"} component={IndexPage} />
                 <Route exact path={"/index.html"} component={IndexPage} />
                 <Route path={"/Apps.html"} component={AppPage} />
-            </Router>
+            </BrowserRouter >
         );
     }
 }
