2f5c5d60c9d4d8af922134944771cb4ccde9641c
kahenteikou <kokkiemouse@gmail.com>
2022-03-22 16:55:40 +0900

    drawer

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,11 +1,21 @@
-import * as React from "react";
-class IndexPage extends React.Component{
-    public render():React.ReactNode{
-        return(
-            <>
-            <h1>Index Page!</h1>
-            </>
-        )
-    }
+import { Typography } from '@mui/material';
+import * as React from 'react';
+import DrawerSetkun from '../components/DrawerSetkun';
+const ToolbarChkun = () => {
+    return (
+        <Typography variant="h6" noWrap component="div">
+            FukukouSSJouhou
+        </Typography>);
 }
-export default IndexPage;
+export default function IndexPage():React.ReactElement {
+        let arraykun=[];
+        for(let i=0;i<50;i++){
+            arraykun.push(i)
+        }
+        return (
+            <DrawerSetkun toolbarchildren={<ToolbarChkun />} >
+                <h1>Index Page</h1>
+                {arraykun.map((v,i)=><>{v}<br /></>)}
+            </DrawerSetkun>
+        );
+}
