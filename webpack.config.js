1542ed4fea212c48f40baa86f022a0f7784bef67
kahenteikou <kokkiemouse@gmail.com>
2022-03-22 15:59:50 +0900

    added

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "index.js"
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.ts', '.tsx']
+    },
+    devtool: 'source-map',
+    devServer:{
+        port:3000,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        })
+    ],
+}
