fe3541f9c2c569234136da4a744d35b319f88e2c
kahenteikou <kokkiemouse@gmail.com>
2022-03-22 16:24:32 +0900

    multi html

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: '404.html'
         })
     ],
 }
