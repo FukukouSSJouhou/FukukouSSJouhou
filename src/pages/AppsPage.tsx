67145a3d60f4b3dd0afaef99c5637f3d573a8434
kahenteikou <kokkiemouse@gmail.com>
2022-03-23 15:37:09 +0900

    addec
    plamar

--- src/pages/AppsPage.tsx
+++ src/pages/AppsPage.tsx
@@ -1,15 +1,15 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Link, Stack, Typography } from "@mui/material";
 import * as React from "react";
 import DrawerSetkun from '../components/DrawerSetkun';
 export default function AppsPage(): React.ReactElement {
-    return (
-        <DrawerSetkun title="福島高校SS部情報班">
+  return (
+    <DrawerSetkun title="福島高校SS部情報班">
       <main>
         {/* Hero unit */}
         <Box
           sx={{
             bgcolor: 'background.paper',
-            backgroundImage:"url(\"images/1500x500.jpg\")",
+            backgroundImage: "url(\"images/1500x500.jpg\")",
             pt: 8,
             pb: 6,
           }}
@@ -25,13 +25,47 @@ export default function AppsPage(): React.ReactElement {
               Applications
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                班で作ったアプリケーションです。<br />
-                下に一覧があります。
+              班で作ったアプリケーションです。<br />
+              下に一覧があります。
             </Typography>
           </Container>
         </Box>
-        </main>
-        
-        </DrawerSetkun>
-    );
+      </main>
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Card sx={{ maxWidth: 500 }} >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                sx={{ width: 151 }}
+                image="/images/plamar.png"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  PLAMAR
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  福島高校校舎の3DマップをAR表示したりするアプリです。
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Link underline="none" href="https://market.android.com/details?id=com.PLAMAR2">
+              <Button size="small" color="primary">
+                PLAY STORE
+              </Button>
+              </Link>
+            </CardActions>
+          </Card>
+        </Container>
+      </Box>
+    </DrawerSetkun>
+  );
 }
