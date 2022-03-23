a853cfdd55de01e128480ac82cdb2e60bf208636
kahenteikou <kokkiemouse@gmail.com>
2022-03-23 16:08:32 +0900

    resp

--- src/components/DrawerSetkun.tsx
+++ src/components/DrawerSetkun.tsx
@@ -39,16 +39,17 @@ export default function DrawerSetkun(props: Props): React.ReactElement {
         setAnchorElNav(event.currentTarget);
     };
 
-    const handleCloseNavMenu = (pagekun:string) => {
-        console.log(pagekun)
-    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+      };
 
     return (
         <>
             <AppBar position="static">
                 <Container maxWidth="xl">
                     <Toolbar disableGutters>
-                        <Link
+                        <Link underline="none" 
                             variant="h6"
                             noWrap style={{ color: "white" }}
                             href="/"
@@ -56,21 +57,59 @@ export default function DrawerSetkun(props: Props): React.ReactElement {
                         >
                             {props.title}
                         </Link>
-
+                        
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem>
+                <Link href={page[1]} underline="none"  >
+                  <Typography textAlign="center">{page[0]}</Typography>
+                </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
                         <Typography
                             variant="h6"
                             noWrap
                             component="div"
                             sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
                         >
-                            a
+                            {props.title}
                         </Typography>
                         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                             {pages.map((page) => (
-                                <Link href={page[1]} style={{color:"white"}}>
+                                <Link href={page[1]} style={{color:"white"}} underline="none" >
                                 <Button
                                     key={page[0]}
-                                    onClick={() => handleCloseNavMenu(page[1])}
+                                    onClick={ handleCloseNavMenu}
                                     sx={{ my: 2, color: 'white', display: 'block' }}
                                 >
                                     
