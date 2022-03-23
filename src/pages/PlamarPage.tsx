34781d5e5f900405d8c0681a9116a37a34c623fc
kahenteikou <kokkiemouse@gmail.com>
2022-03-23 17:40:01 +0900

    added plamar

--- src/pages/PlamarPage.tsx
+++ src/pages/PlamarPage.tsx
@@ -1,70 +1,48 @@
-import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Link, Stack, Typography } from "@mui/material";
+import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Link, Paper, Stack, Typography } from "@mui/material";
 import * as React from "react";
 import DrawerSetkun from '../components/DrawerSetkun';
-export default function PlamarPage():React.ReactElement{
+export default function PlamarPage(): React.ReactElement {
     return (
         <DrawerSetkun title="福島高校SS部情報班">
-          <main>
-            {/* Hero unit */}
-            <Box
-              sx={{
-                bgcolor: 'background.paper',
-                backgroundImage: "url(\"images/1500x500.jpg\")",
-                pt: 8,
-                pb: 6,
-              }}
-            >
-              <Container maxWidth="sm">
-                <Typography
-                  component="h1"
-                  variant="h2"
-                  align="center"
-                  color="text.primary"
-                  gutterBottom
+            <main>
+                {/* Hero unit */}
+                <Box
+                    sx={{
+                        bgcolor: '#d6d6d6',
+                        pt: 8,
+                        pb: 6,
+                    }}
                 >
-                  Applications
-                </Typography>
-                <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                  班で作ったアプリケーションです。<br />
-                  下に一覧があります。
-                </Typography>
-              </Container>
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            PLAMAR
+                        </Typography>
+                        <Typography variant="h6" align="center" color="text.secondary" paragraph>
+                            福島高校校舎の3DマップをAR表示するアプリです。
+                        </Typography>
+                    </Container>
+                </Box>
+            </main>
+            <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    pt: 8,
+                    pb: 6,
+                }}
+            >
+                <Container maxWidth="sm">
+                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                            申し訳ございません。<br />
+                        当ページは工事中です。<br /><br /><br />
+                            <Link href="/" underline='hover'>トップページ {">"}</Link>
+                        </Typography>
+                </Container>
             </Box>
-          </main>
-          <Box
-            sx={{
-              bgcolor: 'background.paper',
-              pt: 8,
-              pb: 6,
-            }}
-          >
-            <Container maxWidth="sm">
-              <Card sx={{ maxWidth: 500 }} >
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    sx={{ width: 151 }}
-                    image="/images/plamar.png"
-                    alt="green iguana"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      PLAMAR
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      福島高校校舎の3DマップをAR表示したりするアプリです。
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Link underline="none" target="_blank" rel="noopener noreferrer" href="https://market.android.com/details?id=com.PLAMAR2">
-                  <Button size="small" color="primary">
-                    PLAY STORE
-                  </Button>
-                  </Link>
-                </CardActions>
-              </Card>
-            </Container>
-          </Box>
         </DrawerSetkun>);
 }
